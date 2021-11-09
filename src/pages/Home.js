34d79385cc16cf1f5babb83e711d7d92686cfa57
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2021-11-09 09:36:30 +0700

    add loading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import Columned from 'react-columned'
+import { CircularProgress } from '@mui/material'
 import useGetListPokemon from '../services/useGetListPokemon'
 import Card from '../components/Card'
 
 function Home() {
-  const { pokemons } = useGetListPokemon()
+  const { loading, pokemons } = useGetListPokemon()
 
   return (
     <div className="container">
       <div className="row center-align">
-        <Columned column={4}>
-          {pokemons &&
-            pokemons.map((pokemon) => {
-              return (
-                <div key={pokemon.slug} className="card-list">
-                  <Card slug={pokemon.slug} pokemon={pokemon.slug} />
-                </div>
-              )
-            })}
-        </Columned>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <Columned column={4}>
+            {pokemons &&
+              pokemons.map((pokemon) => {
+                return (
+                  <div key={pokemon.slug} className="card-list">
+                    <Card slug={pokemon.slug} pokemon={pokemon.slug} />
+                  </div>
+                )
+              })}
+          </Columned>
+        )}
       </div>
     </div>
   )
